0f6a09ea0bc2d24051f0d7e3a6e1fca2e4d602e2
Iolantheny <116835723+Iolantheny@users.noreply.github.com>
2023-07-30 18:45:19 +0200

    xxx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,53 @@
 import React from "react";
-import Img from "gatsby-image"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import { useShoppingCart } from "../context";
 import SearchBar from "./SearchBar";
-import { BsBasketFill, BsPersonCircle } from 'react-icons/bs';
+import { BsBasketFill, BsPersonCircle } from "react-icons/bs";
 
 const Navbar = () => {
-    const data = useStaticQuery(
-        graphql`
-            query {
-                file(relativePath: {eq: "mademe.png"}) {
-                    childImageSharp {
-                        fluid{
-                            ...GatsbyImageSharpFluid
-                        }
-                    }
-                }
-                allContentfulShop (filter: { status: { eq: "available" } }) {
-                    edges {
-                        node {
-                            id
-                        }
-                    }
-                }
-            }    
-        `
-    )
+  const data = useStaticQuery(
+    graphql`
+      query {
+        file(relativePath: { eq: "mademe.png" }) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        allContentfulShop(filter: { status: { eq: "available" } }) {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `
+  );
 
-    const { cartQuantity } = useShoppingCart()
+  const { cartQuantity } = useShoppingCart();
 
-    return (
-        <div className="navbar">
-            <Link to="/"><Img fluid={data.file.childImageSharp.fluid} className="navbar__logo" /></Link>
-            <SearchBar />
-            <div className="navbar__link">
-                <Link to="/koszyk" className="basket">
-                    <BsBasketFill />
-                    {cartQuantity !== 0 ?
-                        <div className="basket__quantity">{cartQuantity}</div>
-                    : null}
-                </Link>
-                <Link to="/zaloguj"><BsPersonCircle /></Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <Img fluid={data.file.childImageSharp.fluid} className="navbar__logo" />
+      </Link>
+      <SearchBar />
+      <div className="navbar__link">
+        <Link to="/cart" className="basket">
+          <BsBasketFill />
+          {cartQuantity !== 0 ? (
+            <div className="basket__quantity">{cartQuantity}</div>
+          ) : null}
+        </Link>
+        <Link to="/login">
+          <BsPersonCircle />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
